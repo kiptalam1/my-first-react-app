143eb3e2aa273a1b05a86ebf895302d069c56fb6
adams <adamskiptalam0@gmail.com>
2025-03-03 18:05:30 +0300

    my first greeting component.

---
+++ src/Greeting.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+function Greeting() {
+    return ( 
+        <div>
+            <h1>&dagger;Hello world.&Dagger;</h1>
+            <p>How are you today?</p>
+        </div>
+    )
+}
+
+export default Greeting;

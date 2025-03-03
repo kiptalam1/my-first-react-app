4fcb6ee47877039cccbdbc13d8ca554396a7b833
adams <adamskiptalam0@gmail.com>
2025-03-03 18:18:05 +0300

    Converted html to jsx.

---
+++ src/Syntax.jsx
@@ -0,0 +1,14 @@
+function Syntax () {
+    return (
+        <div>
+            <h1>Test title</h1>
+            <svg>
+            <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+            </svg>
+            <form>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+export default Syntax;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import Greeting from './Greeting.jsx';
+import Syntax from './Syntax.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    <Syntax />
   </StrictMode>,
 )

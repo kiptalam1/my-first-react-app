862a7b7ff1427f44f309075d5470fde6ad3a2f69
adams <adamskiptalam0@gmail.com>
2025-03-03 19:07:44 +0300

    Comment Greeting import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import Greeting from './Greeting.jsx';
+// import Greeting from './Greeting.jsx';
 import Syntax from './Syntax.jsx'
 
 createRoot(document.getElementById('root')).render(
